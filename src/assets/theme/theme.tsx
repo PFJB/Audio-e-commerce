c6693aa061ed7b284f8437c9d46f8a8dde9e244b
PFJB <jbarbou@outlook.fr>
2023-12-26 18:01:33 +0100

    add theme.tsx

---
+++ src/assets/theme/theme.tsx
@@ -0,0 +1,55 @@
+const colors = {
+    black: "hsl(0° 0% 0%)",
+    blackL: "hsl(0° 0% 6%)",
+    brown: "hsl(22° 65% 57%)",
+    lightBrown: "hsl(21° 94% 75%)",
+    grey: "hsl(0° 0% 95%)",
+    lightGrey: "hsl(0° 0% 98%)",
+    white: "hsl(0° 0% 100%)",
+}
+
+const fonts = {
+    family: {
+        manrope: "'Manrope', sans-serif",
+    },
+    weigth: {
+        regular: 400,
+        medium: 500,
+        bold: 700,
+    },
+    lineSpace: {
+        line_s0: "19px",
+        line_s1: "24px",
+        line_s2: "25px",
+        line_s3: "33px",
+        line_s4: "36px",
+        line_s5: "38px",
+        line_s6: "44px",
+        line_s7: "58px",
+    },
+    charSpace: {
+        char_s0: "1px",
+        char_s1: "1.15px",
+        char_s2: "1.3px",
+        char_s3: "1.5px",
+        char_s4: "1.7px",
+        char_s5: "2px",
+        char_s6: "10px",
+    },
+    size: {
+        font_s0: "13px",
+        font_s1: "14px",
+        font_s2: "15px",
+        font_s3: "18px",
+        font_s4: "24px",
+        font_s5: "28px",
+        font_s6: "32px",
+        font_s7: "40px",
+        font_s8: "56px",
+    },
+}
+
+export const theme = {
+    colors,
+    fonts
+}
