27635e4521ad1ff50833993764b5d38eb6a86ead
PFJB <jbarbou@outlook.fr>
2023-12-26 22:27:09 +0100

    header >basic responsive style

--- src/assets/composant/home/header/Header.tsx
+++ src/assets/composant/home/header/Header.tsx
@@ -1,31 +1,40 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { theme } from "../../../theme/theme";
+import { refreshPage } from "../../../../utils/window";
+const CART = "/images/shared/desktop/icon-cart.svg"
+const LOGO = "/images/shared/desktop/logo.svg"
+const BURGER = "/images/shared/tablet/icon-hamburger.svg"
+
 
 export default function Header() {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const open = () => { setIsOpen(isOpen ? false : true) }
+
     return (
-        <HeaderStyled className="header">
+        <HeaderStyled className="header" $isOpen={isOpen}>
             <nav>
-                <img className="burger" src="../../../../public/images/shared/tablet/icon-hamburger.svg" alt="" />
-                <div className="logo">
-                    <img src="/images/shared/desktop/logo.svg" alt="Logo audiophile" />
-                </div>
+                <img className="burger" onClick={open} src={BURGER} alt="burger button" />
+                <img className="logo" onClick={refreshPage} src={LOGO} alt="Logo audiophile" />
                 <ul className="nav-list">
-                    <li>Home</li>
-                    <li>Headphones</li>
-                    <li>Speakers</li>
-                    <li>Earphones</li>
+                    <li className="navigation_title">Home</li>
+                    <li className="navigation_title">Headphones</li>
+                    <li className="navigation_title">Speakers</li>
+                    <li className="navigation_title">Earphones</li>
                 </ul>
-                <div className="cart"><img src="/images/shared/desktop/icon-cart.svg" alt="" /></div>
+                <img className="cart" src={CART} alt="Cart button" />
             </nav>
         </HeaderStyled>
     )
 }
 
 const HeaderStyled = styled.header`
-display: flex;
-justify-content: center;
 
-height: 90px;
-width: 100%;
+    display: flex;
+    justify-content: center;
+    height: 90px;
+    width: 100%;
     background-color: black;
 
 
@@ -43,28 +52,36 @@ nav {
     .cart{
         position: absolute;
         right: 24px;
+        place-self: center;
     }
     .logo{
-        background-color: red;
         position: absolute;
         left: 50%;
         transform: translateX(-50%);
+        cursor: pointer;
     }
     .nav-list{
-        display: none;
-        color: white;
-        transition: all 1s;
+        display: ${({ $isOpen }) => $isOpen ? "flex" : "none"};
+        position: absolute;
+        width: 100%;
+        height: 340px;
+        left: 0;
+        top: 100%;
+        color: black;
+
+        .navigation_title{
+            font-size: ${theme.fonts.size.font_s0};
+            font-weight: ${theme.fonts.weigth.bold};
+            letter-spacing: ${theme.fonts.charSpace.char_s5};
+        }
     }
 }
 
 @media screen and (min-width: 425px) {
-    nav {
-        .logo{
+    nav .logo{
             position: relative;
             left: 0;
             transform: none;
-        }
-
     }
 }
 
@@ -75,15 +92,16 @@ nav {
         }
         ul{
             display: block;
-            background-color: orange;
         }
 
         .nav-list{
-            display: flex;
             position: absolute;
+            display: flex;
+            width: auto;
+            height: auto;
+            top: 50%;
             left: 50%;
-            transform: translateX(-50%);
-
+            transform: translate(-50%, -50%);
             text-transform: uppercase;
             gap: 34px;
             color: white;
