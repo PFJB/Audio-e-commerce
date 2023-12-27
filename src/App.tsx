d3ae958e9beaea1fe3b2c749c1c5e0c10fd0f441
PFJB <jbarbou@outlook.fr>
2023-12-27 11:53:54 +0100

    main & app > install router system

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-
-import './App.css'
 import Home from './assets/composant/home/Home'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={} />
+
+      {/* <Route path="*" element={<ErrorPage />} /> */}
+    </ Routes>
   )
 }
 
