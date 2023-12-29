2cbbb01fd3ba3b92f84dccc9a6b701cc0c1a42ba
PFJB <jbarbou@outlook.fr>
2023-12-29 14:10:50 +0100

    Button > all version Styled without "onClick

--- src/reusable-ui/button/Button.tsx
+++ src/reusable-ui/button/Button.tsx
@@ -1,14 +1,77 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { theme } from "../../assets/theme/theme";
+import { FaChevronRight } from "react-icons/fa6";
 
-export default function Button({ label }: { label: string }) {
+
+type ButtonProps = {
+    label: string,
+    version?: 1 | 2 | 3,
+}
+
+export default function Button({ label, version = 1 }: ButtonProps) {
     return (
-        <ButtonStyled>
-            {label}
+        <ButtonStyled version={version}>
+            {label} {version === 3 && <span><FaChevronRight /></span>}
         </ButtonStyled>
     )
 }
 
 
 const ButtonStyled = styled.button`
-  
+    width: 160px;
+    height: 48px;
+    cursor: pointer;
+
+    font-size: ${theme.fonts.size.font_s0};
+    font-weight: ${theme.fonts.weigth.bold};
+    letter-spacing: ${theme.fonts.letterSpacing.char_s0};
+
+    text-transform: uppercase;
+
+    span{
+        display: grid;
+        place-content: center;
+    }
+
+    ${({ version }) => getVersion(version)}
 `;
+
+const getVersion = (version: number) => {
+    if (version === 1) return version1Style
+    if (version === 2) return version2Style
+    if (version === 3) return version3Style
+}
+
+const version1Style = css`
+    color: ${theme.colors.white};
+    background-color: ${theme.colors.brown};
+    border: none;
+    &:hover{ background-color: ${theme.colors.lightBrown}; }
+`
+
+const version2Style = css`
+    color: ${theme.colors.black};
+    background-color: ${theme.colors.white};
+    border: 1px solid black;
+    &:hover{
+        color:${theme.colors.white};
+        background-color: ${theme.colors.black};
+    }
+`
+
+const version3Style = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: ${theme.colors.blackL};
+    background: transparent;
+    border: none;
+
+    span{
+        font-size: 0.625rem;
+        margin-left: 0.5rem;
+        color: ${theme.colors.brown};
+    }
+
+    &:hover{ color: ${theme.colors.brown}; }
+`
