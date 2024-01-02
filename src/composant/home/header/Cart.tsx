0964a823d9f0121c642e2f1329ac027d2bb908a0
PFJB <jbarbou@outlook.fr>
2024-01-02 11:34:22 +0100

    add style without Card

--- src/composant/home/header/Cart.tsx
+++ src/composant/home/header/Cart.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Button from "../../../reusable-ui/button/Button";
+import { theme } from "../../../assets/theme/theme";
 
 export default function Cart() {
     return (
@@ -8,11 +9,13 @@ export default function Cart() {
                 <p>{"cart (1)"}</p>
                 <button>remove all</button>
             </div>
-            <div></div>
+            <div>
+
+            </div>
             <div className="">
                 <div className="price">
-                    <p>total</p>
-                    <p>1684.12$</p>
+                    <p className="total">total</p>
+                    <p className="number">1684.12$</p>
                 </div>
                 <Button className="button" label="checkout" version={1} />
             </div>
@@ -38,12 +41,25 @@ const CartStyled = styled.div`
         justify-content: space-between;
         align-items: center;
         height: 60px;
+        p{
+            text-transform: uppercase;
+            color: ${theme.colors.black};
+            font-size: ${theme.fonts.size.font_s3};
+            font-weight: ${theme.fonts.weigth.bold};
+            letter-spacing: ${theme.fonts.letterSpacing.char_s2};
+        }
 
         button{
             border: none;
             background: none;
             padding: 0;
             text-decoration: underline;
+            cursor: pointer;
+            color: ${theme.colors.blackL};
+            font-size: ${theme.fonts.size.font_s2};
+            line-height: ${theme.fonts.lineSpace.line_s2};
+            font-weight: ${theme.fonts.weigth.medium};
+            &:hover, &:focus{ color: ${theme.colors.brown}; }
         }
     }
 
@@ -54,6 +70,21 @@ const CartStyled = styled.div`
         align-items: center;
         padding: 24px 0;
         border-top: solid 1px black;
+        color: red;
+
+        .number{
+            text-transform: uppercase;
+            color: ${theme.colors.black};
+            font-size: ${theme.fonts.size.font_s3};
+            font-weight: ${theme.fonts.weigth.bold};
+        }
+        .total{
+            text-transform: uppercase;
+            color: ${theme.colors.blackL};
+            font-size: ${theme.fonts.size.font_s2};
+            line-height: ${theme.fonts.lineSpace.line_s2};
+            font-weight: ${theme.fonts.weigth.medium};
+        }
     }
 
     .button{
