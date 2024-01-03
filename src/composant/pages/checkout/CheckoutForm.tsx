a0ca358d9ef8fddf766bcadcc2f578a0d6d7d847
PFJB <jbarbou@outlook.fr>
2024-01-03 12:05:20 +0100

    CheckoutForm > styled correction onworking

--- src/composant/pages/checkout/CheckoutForm.tsx
+++ src/composant/pages/checkout/CheckoutForm.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import InputWithTitle from "./inputWithTitle";
 import { theme } from "../../../assets/theme/theme";
+import InputWithTitle from "./InputWithTitle";
 
 export default function CheckoutForm() {
     return (
@@ -8,7 +8,7 @@ export default function CheckoutForm() {
             <h1>checkout</h1>
             <section className="formSection">
                 <h2 className="title">billing details</h2>
-                <div className="inputField">
+                <div className="inputField layout1">
                     <InputWithTitle title={"name"} placeholder={"Alexei Ward"} />
                     <InputWithTitle title={"Email Address"} placeholder={"alexei@mail.com"} />
                     <InputWithTitle title={"Phone Number"} placeholder={"+1 202-555-0136"} />
@@ -16,23 +16,34 @@ export default function CheckoutForm() {
             </section>
             <section className="formSection">
                 <h2 className="title">shipping info</h2>
-                <div className="inputField">
-                    <InputWithTitle title={"Your Address"} placeholder={"1137 Williams Avenue"} />
+                <div className="inputField  layout1">
+                    <InputWithTitle className={"span2"} title={"Your Address"} placeholder={"1137 Williams Avenue"} />
                     <InputWithTitle title={"ZIP Code"} placeholder={"10001"} />
                     <InputWithTitle title={"City"} placeholder={"New York"} />
                     <InputWithTitle title={"Country"} placeholder={"United States"} />
                 </div>
 
             </section>
-            <section className="formSection">
+            <section className="formSection layout2">
                 <h2 className="title">payment details</h2>
                 <div className="inputField">
-                    <InputWithTitle title={"e-Money Number"} placeholder={"238521993"} />
-                    <InputWithTitle title={"e-Money PIN"} placeholder={"6891"} />
+                    <div className="ratio">
+                        <input type="radio" id="e-Money" name="payment" value="e-Money" checked />
+                        <label htmlFor="e-Money">e-Money</label>
+                    </div>
+                    <div className="ratio">
+                        <input type="radio" id="cash" name="payment" value="cash" />
+                        <label htmlFor="cash">Cash on Delivery</label>
+                    </div>
+
                 </div>
+                <InputWithTitle title={"e-Money Number"} placeholder={"238521993"} />
+                <InputWithTitle title={"e-Money PIN"} placeholder={"6891"} />
+
+
             </section>
 
-        </CheckoutFormStyled>
+        </CheckoutFormStyled >
     )
 }
 
@@ -42,7 +53,9 @@ const CheckoutFormStyled = styled.section`
     gap: 32px;
     padding: 23px;
     border-radius: ${theme.radius.light};
-    background-color: white;    
+    background-color: white;
+    max-width: 730px;
+
     h1{
         text-transform: uppercase;
         color: ${theme.colors.black};
@@ -65,6 +78,77 @@ const CheckoutFormStyled = styled.section`
         .inputField{
             display: grid;
             gap: 24px;
+            color: black;
+
+
+          .ratio{
+            position: relative;
+            display: flex;
+            align-items: center;
+            padding: 0 16px;
+            border: 1px solid hsl(0, 0%, 81%);
+            color: #000000;
+            border-radius: ${theme.radius.light};
+            font-size: ${theme.fonts.size.font_s1};
+            font-weight: ${theme.fonts.weigth.bold};
+            gap: 16px;
+            height: 56px;
+            width: 100%;
+
+  
+            input[type=radio]{
+         
+                display: grid;
+                place-content: center;
+                padding: 0;
+                appearance: none;
+                background-color: #fff;
+                margin: 0;
+                appearance: none;
+                color: currentColor;
+                aspect-ratio: 1/1;
+                width: 20px;
+                height: 20px;
+                border: 1px solid gray;
+                border-radius: 50%;
+
+            }
+            input[type=radio]::before {
+                content: "";
+                width: 10px;
+                height: 10px;
+                border-radius: 50%;
+                transform: scale(0);
+                transition: 120ms transform ease-in-out;
+                box-shadow: inset 10px 10px #D87D4A;
+
+            }
+            input[type=radio]:checked::before {
+                transform: scale(1);
+            }
+
+            label{
+                display: flex;
+                align-items: center;
+                width: 100%;
+                height: 100%;
+            }
+          }
         }
+
     }
+
+@media (min-width: 426px){
+    .layout1{
+        grid-template-columns: 1fr 1fr;
+    }
+    .span2{
+        grid-area: auto /span 2;
+    }
+    .layout2{
+        grid-template-columns: 1fr 1fr;
+    }
+
+}
+
 `;
--- src/composant/pages/checkout/InputWithTitle.tsx
+++ src/composant/pages/checkout/InputWithTitle.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 import { theme } from "../../../assets/theme/theme";
 
-export default function InputWithTitle({ title, placeholder }) {
+type inputText = {
+    title: string,
+    placeholder: string,
+    className?: string
+}
+
+export default function InputWithTitle({ title, placeholder, className }: inputText) {
     return (
-        <InputWithTitleStyled>
+        <InputWithTitleStyled className={className}>
             <p>{title}</p>
             <input className="inputText" type="text" placeholder={placeholder} />
         </InputWithTitleStyled>
@@ -15,13 +21,11 @@ const InputWithTitleStyled = styled.div`
     display: grid;
     gap: 9px;
     p{
-        
         text-transform: capitalize;
         color: ${theme.colors.black};
         font-weight: ${theme.fonts.weigth.bold};
         letter-spacing: -0.21px;
         font-size: 12px;
-   
     }
 
     .inputText{
@@ -29,8 +33,9 @@ const InputWithTitleStyled = styled.div`
         height: 56px;
         padding: 0 24px;
         border: 1px solid hsl(0, 0%, 81%);
+        font-size: ${theme.fonts.size.font_s1};
         color: #000000;
         border-radius: ${theme.radius.light};
     }
-`;
 
+`;
